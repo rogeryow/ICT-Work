e80cf2588d80fb24ebb1e662b4982c48aee6ccad
agabriel.nieve <agabriel.nieve@gmail.com>
2020-03-09 16:36:21 +0800

    adding new changes

--- dtr.php
+++ dtr.php
@@ -7,8 +7,13 @@
 <body>
 	<div class="wrapper">
 		<button class="btn" id="btnGetUnixTime">Get Unix Time Now</button>
+		<ul class="list" id="elListUnix"></ul>
+	</div>
+	
+	<div class="wrapper">
+		<button class="btn" id="btnConvertUnixTime">Convert DTR To Unix Time</button>
 		<input class="inpDtr" type="input" name="" placeholder="Enter DTR Schedule">
-		<ul id="elList"></ul>
+		<ul class="list" id="elListConvert"></ul>
 	</div>
 
 	<script src="js/dtr.js" type="text/javascript"></script>
--- js/dtr.js
+++ js/dtr.js
@@ -1,12 +1,20 @@
-const elList = document.getElementById('elList')
+const elListUnix = document.getElementById('elListUnix')
 const btnGetUnixTime = document.getElementById('btnGetUnixTime')
+const btnConvertUnixTime = document.getElementById('btnConvertUnixTime')
+let inpDTR = document.getElementById('inpDtr')
 
-btnGetUnixTime.addEventListener('click', function() {
+btnGetUnixTime.addEventListener('click', displayUnixNowAndDate)
+btnConvertUnixTime.addEventListener('click', displayConvertedUnix)
+
+function displayConvertedUnix() {
+	console.log(inpDTR.value)
+}
+
+function displayUnixNowAndDate() {
 	let unixTime = getUnixTime()
 	let convertedUnixTime = convertUnixtimeToDate(unixTime)
-
-	addToList(`${unixTime} - ${convertedUnixTime}`)
-})
+	addToList(`${unixTime} - ${convertedUnixTime}`, elListUnix)
+}
 
 function getUnixTime() {
 	return Math.round((new Date()).getTime() / 1000)
@@ -16,9 +24,17 @@ function convertUnixtimeToDate(unixTime) {
 	return new Date(unixTime * 1000)
 }
 
-
-function addToList(string) {
+function addToList(string, ul) {
 	let listItem = document.createElement('li')
 	listItem.innerHTML = string
-	elList.appendChild(listItem)
+	ul.appendChild(listItem)
+	ul.scrollIntoView(false)
+	ul.scrollTop = ul.scrollHeight
 }
+
+
+/*
+	morning-in: 5:00 - 8:00, late: 8:01 - 10:00 
+	morning-out:
+
+*/
