0a771db07481e1ffeb3f480c16bf7fe18fcb49c0
jakeaboyot <jakeaboyot0@gmail.com>
2020-03-16 16:02:14 +0800

    adding changes

--- js/dtrUltima.js
+++ js/dtrUltima.js
@@ -1,9 +1,37 @@
 const ONE_SEC_IN_MILISEC = 1000
 const ONE_MIN_IN_SEC =  60
 
-function getDateToday() {
-	return new Date().toJSON().slice(0,10).replace(/-/g,'.');
+const schedule = {
+	morningIn:    ['7:00', '8:00'],
+	morningOut:   ['12:00', '12:30'],
+	afternoonIn:  ['12:31', '13:00'],
+	afternoonOut: ['17:00', '17:30'],
 }
+
+const testSched = [
+	{
+		date: '3/16/2020',
+		time: [
+			'7:30',
+			'8:05',
+			'8:09',
+			'12:00',
+			'13:00',
+			'13:09',
+			'16:49',
+		]
+	},
+	{
+		date: '3/24/2020',
+		time: [
+			'7:30',
+			'8:15',
+			'8:10',
+			'12:01',
+			'13:35',
+		]
+	}
+]
 
 function convertDateToUnix(date) {
 	date = (typeof date === 'undefined') ? new Date() : date
@@ -28,8 +56,10 @@ function formulaUnixToDate(unix) {
 	return new Date(unix * ONE_SEC_IN_MILISEC)
 }
 
-console.log(
-	convertUnixToDate(
-		convertDateToUnix() + convertMinToUnix()
-	)
-)
+
+function calcuateDTR(schedule, dtr) {
+	console.log(schedule)
+	console.log(dtr)
+}
+
+calcuateDTR(schedule, testSched)
