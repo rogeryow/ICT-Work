26ee6381903d93abad1aa88d6472a9d905ad3408
agabriel.nieve <agabriel.nieve@gmail.com>
2020-03-17 15:11:56 +0800

    major changes

--- js/dtrUltima.js
+++ js/dtrUltima.js
@@ -1,36 +1,67 @@
 const ONE_SEC_IN_MILISEC = 1000
 const ONE_MIN_IN_SEC =  60
 
-const schedule = {
+const workSched = {
 	morningIn:    ['7:00', '8:00'],
 	morningOut:   ['12:00', '12:30'],
 	afternoonIn:  ['12:31', '13:00'],
 	afternoonOut: ['17:00', '17:30'],
 }
 
-const testSched = [
+const userSched = [
 	{
-		date: '3/16/2020',
-		record: [
-			{time: '7:30'},
-			{time: '8:05'},
-			{time: '8:09'},
-			{time: '12:00'},
-			{time: '13:00'},
-			{time: '13:09'},
-			{time: '16:49'},
+		ID: '101', dtr:
+		[ 
+			{
+				date: '3/18/2020',
+				record:[
+					{time: '7:30'},
+					{time: '8:05'},
+					{time: '8:09'},
+					{time: '12:00'},
+					{time: '13:00'},
+					{time: '13:09'},
+					{time: '16:49'},
+				]
+			},
+			{
+				date: '3/25/2020',
+				record: [
+					{time: '7:30'},
+					{time: '8:15'},
+					{time: '8:10'},
+					{time: '12:01'},
+					{time: '13:35'},
+				]
+			}
 		]
 	},
+
 	{
-		date: '3/24/2020',
-		record: [
-			{time: '7:30'},
-			{time: '8:15'},
-			{time: '8:10'},
-			{time: '12:01'},
-			{time: '13:35'},
+		ID: '102', dtr:
+		[ 
+			{
+				date: '3/16/2020',
+				record:[
+					{time: '7:31'},
+					{time: '8:07'},
+					{time: '8:20'},
+					{time: '12:30'},
+					{time: '13:01'},
+				]
+			},
+			{
+				date: '3/24/2020',
+				record: [
+					{time: '7:30'},
+					{time: '8:15'},
+					{time: '8:10'},
+					{time: '12:21'},
+					{time: '13:45'},
+				]
+			}
 		]
-	}
+	},
 ]
 
 function convertDateToUnix(date) {
@@ -56,20 +87,24 @@ function formulaUnixToDate(unix) {
 	return new Date(unix * ONE_SEC_IN_MILISEC)
 }
 
+function pushUnixToDtr(userSched) {
+	let userEntries = userSched.entries()
+	for (const [index, value] of userEntries) {
 
-function calcuateDTR(schedule, dtr) {
-	for (const [index, value] of dtr.entries()) {
-		if(dtr[index].hasOwnProperty('record')) {
+		let entry = Object.entries(value['dtr'])
+		for (const [key, value] of entry) {
 
-			dtr[index]['record'].forEach((row, i) => {
-				if(row.hasOwnProperty('time')) {
-					dtr[index]['record'][i]['unix'] = 'nice'
-				} else return 
-			})
+			let date = value['date']
+			let records = Object.entries(value['record'])
+			for (const [key, value] of records) {
+				let timestamp = `${date} ${value['time']}`
+				value['unix'] = convertDateToUnix(new Date(timestamp))
+			}
 
-		} else return 
+		}
+
 	}
-	console.log(dtr)
 }
 
-calcuateDTR(schedule, testSched)
+pushUnixToDtr(userSched)
+console.log(userSched)
